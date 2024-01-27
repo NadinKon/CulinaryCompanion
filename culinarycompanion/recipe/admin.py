607754ee737ab4eed1c26c4a58eb70e8beb3c98f
NadinKon <nadinkahf@gmail.com>
2024-01-27 14:13:15 +0300

    add InlineModelAdmin

--- culinarycompanion/recipe/admin.py
+++ culinarycompanion/recipe/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from .models import Product, Recipe, RecipeProduct
 
+
+class RecipeProductInline(admin.TabularInline):
+    model = RecipeProduct
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'times_cooked')
 
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeProductInline]
     list_display = ('name',)
 
 
 @admin.register(RecipeProduct)
 class RecipeProductAdmin(admin.ModelAdmin):
     list_display = ('recipe', 'product', 'weight')
-
